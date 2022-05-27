eb4bfdf15d3b100080e7c1d3448a69f4aa5f2c17
usman.hussain <usman.hussain@avanzainnovations.com>
2022-05-27 18:30:26 +0500

    Config updated

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x00265833E0AEE8b36C6089c9B6497aa356a1Eb33"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
